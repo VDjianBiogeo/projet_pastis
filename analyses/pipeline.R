7d451f9766197e0825474dcc2aa6fbc5b8c509cb
Titouan Dionet <titouan.dionet@univ-lorraine.fr>
2024-12-05 16:17:24 +0100

    Update description and target.

--- analyses/pipeline.R
+++ analyses/pipeline.R
@@ -6,21 +6,25 @@ tar_source()
 
 # Defining pipeline ----
 list(
-  ## Cities data ----
-  tar_target(name = cities_data, command = read_data(path = here::here("data", "cities.csv"), header = TRUE)),
-  
   ## Meteo data ----
+  ### Read data
   tar_target(name = meteo_data, command = read_data(path = here::here("data", "meteo.txt"), header = TRUE, dec = ",")),
   
-  ## Make a PCA on meteo data ----
+  ### Make a PCA
   tar_target(name = pca_meteo,
              command = factopca(meteo_data, 
                                 "Ville")),
-  
+  ### Plot graph
   tar_target(name = pca_meteo_graph,
-             command = multivariate_plot(pca_meteo))
+             command = multivariate_plot(pca_meteo)),
   
+  ## Survey and Cities data ----
+  ### Read cities data
+  tar_target(name = cities_data, command = read_data(path = here::here("data", "cities.csv"), header = TRUE)),
   
+  ### Read survey data
+  tar_target(name = survey_data, command = read_data(path = here::here("data", "survey_data.csv"), header = TRUE))
   
+  ### Combine data
   
 )
