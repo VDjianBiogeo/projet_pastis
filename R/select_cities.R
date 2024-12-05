778fe38b87ccacd6a32592072a61818fc14d884d
Titouan Dionet <titouan.dionet@univ-lorraine.fr>
2024-12-05 15:19:07 +0100

    Add new function [select_cities].

---
+++ R/select_cities.R
@@ -0,0 +1,12 @@
+#' @title Select cities
+#' 
+#' @description
+#' This function extract cities coordinates in the cities dataset.
+#' 
+#' @export
+#' 
+
+select_cities = function(data) {
+  
+  
+}
