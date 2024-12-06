656d2fc4548fb024f65d0fc3054949c5ef3d5b4e
Titouan Dionet <titouan.dionet@univ-lorraine.fr>
2024-12-06 10:31:18 +0100

    Update pipeline.R

--- analyses/pipeline.R
+++ analyses/pipeline.R
@@ -14,24 +14,27 @@ list(
   
   ### Make a PCA
   tar_target(name = pca_meteo,
-             command = factopca(meteo_data, 
-                                "Ville")),
+             command = factopca(meteo_data, "Ville")),
   ### Plot graph
   tar_target(name = pca_meteo_graph,
              command = multivariate_plot(pca_meteo)),
   
   ## Survey and Cities data ----
   ### Read survey data
-  tar_target(name = survey_data, command = read_data(path = here::here("data", "survey_data.csv"), header = TRUE, dec = ",", sep = ";")),
+  tar_target(name = survey_data, 
+             command = read_data(path = here::here("data", "survey_data.csv"), header = TRUE, dec = ",", sep = ";")),
   
   ### Extract zipcodes
-  tar_target(name = zipcodes, command = unique_zipcodes(data = survey_data, col = 1:(dim(survey_data)[2]-1))),
+  tar_target(name = zipcodes, 
+             command = unique_zipcodes(data = survey_data, col = 1:(dim(survey_data)[2]-1))),
   
   ### Read cities data
-  tar_target(name = cities_data, command = read_data(path = here::here("data", "cities.csv"), header = TRUE)),
+  tar_target(name = cities_data, 
+             command = read_data(path = here::here("data", "cities.csv"), header = TRUE)),
   
   ### Extract cities from zipcodes
-  tar_target(name = cities_data_extract, command = extract_coordinates_from_zipcode(data = cities_data, zipcode_col_name = "zip_code", zipcodes_vec = zipcodes)),
+  tar_target(name = cities_data_extract, 
+             command = extract_coordinates_from_zipcode(data = cities_data, zipcode_col_name = "zip_code", zipcodes_vec = zipcodes)),
   
   ### Combine data
   tar_target(name = childhood_data, command = join_cities_survey(cities_data = cities_data_extract, survey_data = survey_data, type = "childhood")),
@@ -42,8 +45,10 @@ list(
   tar_target(name = west_data,      command = join_cities_survey(cities_data = cities_data_extract, survey_data = survey_data, type = "west")),
   
   ### Create a joined data with all informations
-  tar_target(name = coord_data, command = join_coord_data(data_list = list(childhood_data, present_data, south_data, north_data, east_data, west_data), col_id = "id")),
+  tar_target(name = coord_data, 
+             command = join_coord_data(data_list = list(childhood_data, present_data, south_data, north_data, east_data, west_data), col_id = "id")),
   
   ### Transform data
-  tar_target(name = coord_data_long, command = pivot_data(data = coord_data, col_id = "id"))
+  tar_target(name = coord_data_long, 
+             command = pivot_data(data = coord_data, col_id = "id"))
 )
